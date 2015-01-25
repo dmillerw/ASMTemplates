754d040e1a2e37de5c8ee68533e94fd2b7b9706a
dmillerw <dmillerw@gmail.com>
2015-01-24 22:59:53 -0800

    Removed FML code

--- src/main/java/dmillerw/asm/test/MainMod.java
+++ src/main/java/dmillerw/asm/test/MainMod.java
@@ -1,11 +1,7 @@
 package dmillerw.asm.test;
 
-import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import dmillerw.asm.core.Generator;
-import net.minecraftforge.common.MinecraftForge;
 
-@Mod(modid = "ASM")
 public class MainMod {
 
     public static void main(String[] args) throws Exception {
@@ -26,10 +22,5 @@ public class MainMod {
         if (foo2 instanceof Echo) {
             ((Echo) foo2).echo();
         }
-    }
-
-    @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event) throws Exception {
-        MinecraftForge.EVENT_BUS.register(new EventHandler());
     }
 }
