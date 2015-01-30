75dade2d88bb8aa9b02ede45744c5f78ca0e5df7
dmillerw <dmillerw@gmail.com>
2015-01-29 23:54:17 -0800

    Added javadocs to the MCastParam annotation

--- src/main/java/dmillerw/asm/annotation/MCastParam.java
+++ src/main/java/dmillerw/asm/annotation/MCastParam.java
@@ -9,6 +9,18 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface MCastParam {
 
+    /**
+     * Index of the parameter to be cast. 0 being the first parameter, and so on
+     * Remember that (%CLASS% ... param) arguments are compiled to a simple array param
+     *
+     * Casting the return value should be done with an index of -1
+     */
     int index();
+
+    /**
+     * Class the param or return value should be cast too.
+     *
+     * Should be a fully qualified name, with either . or / path separators
+     */
     String cast();
 }
